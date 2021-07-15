bbaaf8b66977371022c6699c638cea1e1e5c95e0
Ian <yanik.belyakov@gmail.com>
2021-07-15 18:45:55 +0300

    респавн кактусов и краш при столкновении с кактусом

--- game.py
+++ game.py
@@ -1,27 +1,28 @@
-import time
 import pygame
-import sys
 import random
 pygame.init()
-screen = pygame.display.set_mode([1000,300])
+screen = pygame.display.set_mode([1000, 300])
 screen.fill([255, 255, 255])
 player = pygame.image.load("10544.png")
 player = pygame.transform.scale(player, (80, 100))
 y = 150
 b1_x = 1000
 screen.blit(player, [100, y])
-pg = pygame.draw.rect(screen, (230, 230 ,230),(0, 250, 1000,50))
+pg = pygame.draw.rect(screen, (0, 0, 0), (0, 250, 1000, 50))
 space = False
+yk = 0
+wk = 0
+sltime = random.randint(0, 3)
+
 
 def blocs():
     global bl_running
-    bl_h = random.randint(80, 120)
-    bl_w = random.randint(80,120)
-    bl1 = pygame.draw.rect(screen, (225, 0, 0), (b1_x, 200, bl_h, bl_w))
+    global yk
+    global wk
+    bl = pygame.image.load("images.xcf")
+    bl1 = pygame.transform.scale(bl, (wk, 300))
+    screen.blit(bl1, [b1_x, yk])
     return bl1
-
-
-    
 
 
 is_jump = True
@@ -39,6 +40,11 @@ while running:
     if keys[pygame.K_SPACE]:
         print("space")
         space = True
+    a = pygame.Rect((100, y), (8, 100))
+    b = pygame.Rect((b1_x, yk), (wk, 300))
+    if a.colliderect(b):
+        print('краш')
+
     if is_jump and space:
         if jumpCount >= -10:
             y -= (jumpCount * abs(jumpCount)) * 0.5
@@ -48,11 +54,16 @@ while running:
             isJump = False
             space = False
     if bl_running:
-        blocs()
-        b1_x -= 10
+        if b1_x > -100:
+            blocs()
+            b1_x -= 15
+        else:
+            b1_x = 1100
+            yk = random.randint(80, 120)
+            wk = random.randint(70, 140)
+            blocs()
     isJump = True
 
-
-    pg = pygame.draw.rect(screen, (230, 230, 230), (0, 250, 1000, 50))
+    pg = pygame.draw.rect(screen, (0, 255, 0), (0, 250, 1000, 50))
     screen.blit(player, [100, y])
     pygame.display.update()
